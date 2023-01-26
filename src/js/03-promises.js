b57657a6510f3585c412f405af44e986d8600197
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-01-26 18:09:00 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix, { Notify } from 'notiflix';
+
+let initDelay = document.querySelector('input[name="delay"]');
+let initStep = document.querySelector('input[name="step"]');
+let initAmount = document.querySelector('input[name="amount"]');
+const submit = document.querySelector('form');
+
+
+const createPromise = (position, delay) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  let targetDelay = Number(initDelay.value);
+  let targetStep = Number(initStep.value);
+  let targetAmount = Number(initAmount.value);
+  
+  for (let i = 1; i <= targetAmount; i++) {
+    createPromise(i, targetDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    targetDelay += targetStep;
+  }
+};
+
+submit.addEventListener('submit', submitHandler);
