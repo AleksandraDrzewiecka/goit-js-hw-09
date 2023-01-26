31d123e3b8fb43ac6e29b3ab2dee4c133c0e0fea
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-01-26 18:52:37 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,8 @@ const stopBtn = document.querySelector('[data-stop');
 
 let interval;
 
+stopBtn.disabled = true;
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
@@ -11,15 +13,18 @@ const changeColor = () => {
     let color = getRandomHexColor();
     document.querySelector('body').style.backgroundColor = color;
 };
+ 
 
 const start = () => {
     startBtn.disabled = true;
+    stopBtn.disabled = false;
     interval = setInterval(changeColor, 1000)
 };
 
 const stop = () => {
     clearInterval(interval);
     startBtn.disabled = false;
+    stopBtn.disabled = true;
 }
 
 startBtn.addEventListener('click', start);
